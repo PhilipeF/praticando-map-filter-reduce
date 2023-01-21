8f45685e6ee27ca760ede4091c4be3de5a3cc02a
rick <philipesferreiraa@gmail.com>
2023-01-21 14:00:09 -0300

    exercicio 01 - ok

---
+++ 01.js
@@ -0,0 +1,26 @@
+/*
+  01
+
+  - Gere um novo array com apenas os números ímpares do array abaixo e exiba 
+    o novo array no console.
+
+  Dica: pesquise por remainder operator (%).
+*/
+
+const randomNumbers = [10, 30, 15, 25, 50, 40, 5]
+
+function verificaNumbers(value) {
+    return value % 2 != 0
+}
+
+const imparNumbers = randomNumbers.filter(verificaNumbers)
+
+console.log(imparNumbers)
+
+
+
+/* 
+Quando eu devo usar o filter: 
+
+Quando preciso de um novo array com a quantidade itens menor do q do array original
+*/
